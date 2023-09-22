bb7e66366fffcfe5cdcdea0f31d4361fe922f701
Aleksandr <smoothsatori@gmail.com>
2023-09-22 15:29:58 +0300

    add lesson 3.4

---
+++ Good, kind Python - a training course from Sergey Balakirev/3.4_Special_Characters_and_Escaping/3.4_lesson_task_5.py
@@ -0,0 +1,11 @@
+# Подвиг 5. Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts.
+# Результат отобразите на экране.
+# ====================
+# Sample Input:
+# ====================
+# Sample Output:
+# C:\WINDOWS\System32\drivers\etc\hosts
+
+
+a = r"C:\WINDOWS\System32\drivers\etc\hosts"
+print(a)
