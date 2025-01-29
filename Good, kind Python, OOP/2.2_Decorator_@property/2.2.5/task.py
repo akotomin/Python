96ef25af5ccc1b64cc51d9a25be0b0fd51ac8765
alextomtomo <luminevolve@gmail.com>
2025-01-29 07:23:53 +0300

    Diffclt task added

--- Good, kind Python, OOP/2.2_Decorator_@property/2.2.5/task.py
+++ Good, kind Python, OOP/2.2_Decorator_@property/2.2.5/task.py
@@ -16,7 +16,7 @@ class WindowDlg:
 
     @property
     def height(self):
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, height):
@@ -26,20 +26,3 @@ class WindowDlg:
 
     def show(self):
         return print(f"{self.__title}: {self.__width}, {self.__height}")
-
-
-wnd = WindowDlg('Окно', 10, 11)
-# assert '_WindowDlg__title' in wnd.__dict__ and '_WindowDlg__width' in wnd.__dict__ and '_WindowDlg__height' in wnd.__dict__, \
-#     "Атрибуты в экземпляре класса не совпадают, так же они должны быть защищёнными"
-#
-# assert wnd._WindowDlg__title == 'Окно' and type(wnd._WindowDlg__title) is str, 'Название должно быть строкой'
-# assert 'width' in dir(wnd) and 'height' in dir(wnd), 'В классе должны быть методы для обращения к приватным атрибутам'
-#
-# assert wnd.width == 10, 'Геттер для __width работает неправильно'
-# wnd.width = 11
-# assert wnd.width == 11, 'Сеттер для __width работает неправильно'
-#
-assert wnd.height == 11, 'Геттер для __height работает неправильно'
-wnd.height = 22
-print(wnd.height, wnd.__dict__, wnd.height == 22)
-assert wnd.height == 22, 'Сеттер для __height работает неправильно'
