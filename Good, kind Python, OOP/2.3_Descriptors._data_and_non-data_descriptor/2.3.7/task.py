1817c4202b939e94ba1f44fde6340e21785eaefe
alextomtomo <luminevolve@gmail.com>
2025-02-01 11:41:53 +0300

    Added new tasks to the OOP course

---
+++ Good, kind Python, OOP/2.3_Descriptors._data_and_non-data_descriptor/2.3.7/task.py
@@ -0,0 +1,35 @@
+class FloatValue:
+    @classmethod
+    def verify_coord(cls, coord):
+        if type(coord) != float:
+            raise TypeError("Присваивать можно только вещественный тип данных.")
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        self.verify_coord(value)
+        instance.__dict__[self.name] = value
+
+
+class Cell:
+    value = FloatValue()
+
+    def __init__(self, value=0.0):
+        self.value = value
+
+
+class TableSheet:
+    def __init__(self, N, M):
+        self.cells = [[Cell() for _ in range(M)] for _ in range(N)]
+
+
+table = TableSheet(5, 3)
+count = 0
+for i in table.cells:
+    for k in i:
+        count += 1
+        k.value = float(count)
