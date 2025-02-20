c6832185a1d914c19d823fd73e851774b5c55479
alextomtomo <luminevolve@gmail.com>
2025-02-20 13:00:18 +0300

    Added new tasks

---
+++ Good, kind Python, OOP/3.7_Method_(__bool__)/3.7.5/task.py
@@ -0,0 +1,35 @@
+import sys
+
+class MailItem:
+    def __init__(self, mail_from, title, content):
+        self.mail_from = mail_from
+        self.title = title
+        self.content = content
+        self.is_read = False
+
+    def set_read(self, fl_read):
+        self.is_read = fl_read
+
+    def __bool__(self):
+        return self.is_read
+
+class MailBox:
+    def __init__(self):
+        self.inbox_list = []
+
+    def receive(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))
+        for line in lst_in:
+            mail_from, title, content = line.split('; ')
+            item = MailItem(mail_from, title, content)
+            self.inbox_list.append(item)
+
+
+mail = MailBox()
+mail.receive()
+
+if mail.inbox_list:
+    mail.inbox_list[0].set_read(True)
+    mail.inbox_list[-1].set_read(True)
+
+inbox_list_filtered = list(filter(bool, mail.inbox_list))
