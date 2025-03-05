8489966b56689eccec6e91d458ccfc6677e679d0
alextomtomo <luminevolve@gmail.com>
2025-03-05 08:07:20 +0300

    Added new tasks

---
+++ Good, kind Python, OOP/4.Inheritance_and_polymorphism/4.2_Function_issubclass()._Inheriting_from_built-in_types/4.2.9/task.py
@@ -0,0 +1,10 @@
+class IteratorAttrs:
+    def __iter__(self):
+        return iter(self.__dict__.items())
+
+
+class SmartPhone(IteratorAttrs):
+    def __init__(self, model, size, memory):
+        self.model = model
+        self.size = size
+        self.memory = memory
