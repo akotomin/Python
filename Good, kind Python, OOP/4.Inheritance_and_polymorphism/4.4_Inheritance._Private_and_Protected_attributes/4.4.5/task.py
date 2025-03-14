4d52614ba3974c738eadd0e3ed41978e88db5b07
alextomtomo <luminevolve@gmail.com>
2025-03-14 07:20:46 +0300

    Added new tasks

--- Good, kind Python, OOP/4.Inheritance_and_polymorphism/4.4_Inheritance._Private_and_Protected_attributes/4.4.5/task.py
+++ Good, kind Python, OOP/4.Inheritance_and_polymorphism/4.4_Inheritance._Private_and_Protected_attributes/4.4.5/task.py
@@ -1,13 +1,36 @@
-class Book:
-    def __init__(self, title, author, pages, year):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.year = year
+class Animal:
+    def __init__(self, name, kind, old):
+        self.__name = name
+        self.__kind = kind
+        self.__old = old
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def kind(self):
+        return self.__kind
+
+    @kind.setter
+    def kind(self, kind):
+        self.__kind = kind
+
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, old):
+        self.__old = old
 
 
-class DigitBook(Book):
-    def __init__(self, title, author, pages, year, size, frm):
-        super().__init__(title, author, pages, year)
-        self.size = size
-        self.frm = frm
+animals = [
+    Animal('Васька', 'дворовый кот', 5),
+    Animal('Рекс', 'немецкая овчарка', 8),
+    Animal('Кеша', 'попугай', 3)
+]
